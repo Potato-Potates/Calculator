b81db9ecbcae4b96d76aae6e3202fa6cd217f51d
cwtf <christopherwong@hitachi-ebworx.com>
2017-09-04 18:47:34 +0800

    WIP button handling

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './style.css';
 
+let self = this;
+
 class Button extends React.Component {
+    handleClick() {
+        console.log('value', this.props.value);
+    },
     render() {
-        return <div className="col-3 btn-wrapper">
-            <button type="button" className="btn btn-lg btn-light btn-block">
+        return <div className={this.props.cssClass + ' btn-wrapper'}>
+            <button type="button" className="btn btn-lg btn-light btn-block" onClick={ this.handleClick }>
                 {this.props.label}
             </button>
         </div>;
--- src/Calculator/index.jsx
+++ src/Calculator/index.jsx
@@ -5,25 +5,80 @@ import Button from "../Button";
 import "./style.css";
 
 const btnLabels = [
-    '7', '8', '9', 'x',
-    '4', '5', '6', '-',
-    '1', '2', '3', '+',
-    '0', '.', '='
+    {
+        label: '7',
+        value: 'Seven',
+        cssClass: 'col-3'
+    }, {
+        label: '8',
+        value: 'Eight',
+        cssClass: 'col-3'
+    }, {
+        label: '9',
+        value: 'Nine',
+        cssClass: 'col-3'
+    }, {
+        label: 'x',
+        value: 'Multiply',
+        cssClass: 'col-3'
+    }, {
+        label: '4',
+        value: 'Four',
+        cssClass: 'col-3'
+    }, {
+        label: '5',
+        value: 'Five',
+        cssClass: 'col-3'
+    }, {
+        label: '6',
+        value: 'Six',
+        cssClass: 'col-3'
+    }, {
+        label: '-',
+        value: 'Minus',
+        cssClass: 'col-3'
+    }, {
+        label: '1',
+        value: 'One',
+        cssClass: 'col-3'
+    }, {
+        label: '2',
+        value: 'Two',
+        cssClass: 'col-3'
+    }, {
+        label: '3',
+        value: 'Three',
+        cssClass: 'col-3'
+    }, {
+        label: '+',
+        value: 'Plus',
+        cssClass: 'col-3'
+    }, {
+        label: '0',
+        value: 'Zero',
+        cssClass: 'col-6'
+    }, {
+        label: '.',
+        value: 'Dot',
+        cssClass: 'col-3'
+    }, {
+        label: '=',
+        value: 'Equals',
+        cssClass: 'col-3'
+    }
 ];
-const buttons = btnLabels.map((btnLabel) => 
-    <Button label={btnLabel} />
-);
+const buttons = btnLabels.map((btnLabel) => <Button label={btnLabel.label} cssClass={btnLabel.cssClass}/>);
 
 class Calculator extends React.Component {
     renderButton() {
-        return <Button />;
+        return <Button/>;
     }
 
     render() {
         return <div className="outline">
-            <Textbox />
+            <Textbox/>
             <div className="row">
-                { buttons }
+                {buttons}
             </div>
         </div>
     }
