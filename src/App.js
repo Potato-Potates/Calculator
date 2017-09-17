509c868531a2c9ffb89e7ea87a692c323e1caa22
Ahmad Rafiuddin <ahmadrafiuddin@protonmail.com>
2017-09-17 17:04:47 +0800

    change to single component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Calculator from "./Calculator";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
--- src/Calculator/index.jsx
+++ src/Calculator/index.jsx
@@ -1,91 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Textbox from "../Textbox";
-import Button from "../Button";
-import "./style.css";
-
-const btnLabels = [
-    {
-        label: '7',
-        value: 'Seven',
-        cssClass: 'col-3'
-    }, {
-        label: '8',
-        value: 'Eight',
-        cssClass: 'col-3'
-    }, {
-        label: '9',
-        value: 'Nine',
-        cssClass: 'col-3'
-    }, {
-        label: 'x',
-        value: 'Multiply',
-        cssClass: 'col-3'
-    }, {
-        label: '4',
-        value: 'Four',
-        cssClass: 'col-3'
-    }, {
-        label: '5',
-        value: 'Five',
-        cssClass: 'col-3'
-    }, {
-        label: '6',
-        value: 'Six',
-        cssClass: 'col-3'
-    }, {
-        label: '-',
-        value: 'Minus',
-        cssClass: 'col-3'
-    }, {
-        label: '1',
-        value: 'One',
-        cssClass: 'col-3'
-    }, {
-        label: '2',
-        value: 'Two',
-        cssClass: 'col-3'
-    }, {
-        label: '3',
-        value: 'Three',
-        cssClass: 'col-3'
-    }, {
-        label: '+',
-        value: 'Plus',
-        cssClass: 'col-3'
-    }, {
-        label: '0',
-        value: 'Zero',
-        cssClass: 'col-6'
-    }, {
-        label: '.',
-        value: 'Dot',
-        cssClass: 'col-3'
-    }, {
-        label: '=',
-        value: 'Equals',
-        cssClass: 'col-3'
-    }
-];
-const buttons = btnLabels.map((btnLabel) => <Button
-    key={btnLabel.label}
-    label={btnLabel.label}
-    cssClass={btnLabel.cssClass}
-    value={btnLabel.value}/>);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
 
 class Calculator extends React.Component {
-    renderButton() {
-        return <Button/>;
-    }
+  state = {
+    displayValue: 0
+  }
 
-    render() {
-        return <div className="outline">
-            <Textbox/>
-            <div className="row">
-                {buttons}
+  inputNumber(number) {
+    let { displayValue } = this.state;
+
+    this.setState({
+      displayValue: String(number)
+    });
+  }
+  render() {
+    let { displayValue } = this.state;
+
+    return (
+      <div className="calculator">
+        <div className="calculator-display">{ displayValue }</div>
+        <div className="calculator-keypad">
+          <div className="input-keys">
+            <div className="function-keys">
+              <button className="calculator-key key-clear">AC</button>
+              <button className="calculator-key key-sign">±</button>
+              <button className="calculator-key key-percent">%</button>
             </div>
+            <div className="digit-keys">
+              <button className="calculator-key key-0" onClick={() => this.inputNumber(0)}>0</button>
+              <button className="calculator-key key-dot">●</button>
+              <button className="calculator-key key-1" onClick={() => this.inputNumber(1)}>1</button>
+              <button className="calculator-key key-2" onClick={() => this.inputNumber(2)}>2</button>
+              <button className="calculator-key key-3" onClick={() => this.inputNumber(3)}>3</button>
+              <button className="calculator-key key-4" onClick={() => this.inputNumber(4)}>4</button>
+              <button className="calculator-key key-5" onClick={() => this.inputNumber(5)}>5</button>
+              <button className="calculator-key key-6" onClick={() => this.inputNumber(6)}>6</button>
+              <button className="calculator-key key-7" onClick={() => this.inputNumber(7)}>7</button>
+              <button className="calculator-key key-8" onClick={() => this.inputNumber(8)}>8</button>
+              <button className="calculator-key key-9" onClick={() => this.inputNumber(9)}>9</button>
+            </div>
+          </div>
+          <div className="operator-keys">
+            <button className="calculator-key key-divide">÷</button>
+            <button className="calculator-key key-multiply">×</button>
+            <button className="calculator-key key-subtract">−</button>
+            <button className="calculator-key key-add">+</button>
+            <button className="calculator-key key-equals">=</button>
+          </div>
         </div>
-    }
+      </div>
+    )
+  }
 }
 
 export default Calculator;
